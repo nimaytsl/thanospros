0e0446ef6b339c39eaef70d71b948c2e3600184e
nimaytsl <106383613+nimaytsl@users.noreply.github.com>
2023-01-14 19:31:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,22 +3,23 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 10248430
-    API_HASH = "42396a6ff14a569b9d59931643897d0d"
+    APP_ID = 17674920
+    API_HASH = "4c55df5c8d646c88aa4bbe177761117c"
     # the name to display in your alive message
-    ALIVE_NAME = "rishabh"
+    ALIVE_NAME = "Plexitics"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://jrycfssc:fEZMYWiOcO9vB8cIq1_Z97GhW3TiTppJ@tiny.db.elephantsql.com/jrycfssc"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "your session"
+    STRING_SESSION = "BQBQeqhDKj0Afpt9cH4u_C48Ken7lfgi3HyJnszr-aASSkvEDXRWRIhO1FqKJhJb11B7kOykAJyIH7ghFbYIVP82He38DmgRu7qN-wUYexJ3p-HR62mT7DWGs_UfS1-lpPUjofRxQ3B5xwiqj9nNr5I_q_uB0vbPD5WuKbvt15TpvQzDrS0UdKEITphs_YDDI25T0gdS-TstZyBUPk7LlmVOjyjBx_VLO8cqfhif1P5oeSZnfpcir6Dd32x7boWGEZS5w9EKufqrJjoc2-2-9c2q0cU9cFPZaRGhn4bsy5qxtMl9_A1JKCSJpOh9mlo09sUb3LAFUngS1nxU3NX7wb6eAAAAAUAyzEEA"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "your token"
+    TG_BOT_TOKEN = "5856053493:AAHpbSwhYWHsTPkyOWnx4QSPqGVn5fsDibE"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID =
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "your 2nd id telethon session" #note don't use maim account to vc player
+    VC_SESSION = "BQAm39EMu4_U18sHhhiZRaZhci7mPg3ZlIxy217GYi886awYXNB-FIyspPZduPVuVd_6GtJqsVh0HuxpLFHsqKAUBAD2UJKBFb-Xl3YR7mfzKN12zNrwYWiQP5aVx0cT9nAqoQPuNVFZJgKuxW9ypHGJS9n_SVwY_mR93kF-L-aZh7kelfNBikXDN_9KG3Uk6p6NQ01P59nBEYcf-IsTPlRuaUIJTt_e-uGaTQ0pNiIq2yFIBmE7SQYIpDKxqFZgkaq2QGJEfPusiAO8Pbaoadt3K1Z8kq3-ImCUa4j3TwN8evNFuSzcu69tWX4DdQ3F5O31srIKowZcYMOiGubmFEE2AAAAAV23WXsA
+" #note don't use maim account to vc player
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
